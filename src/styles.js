0f84335704d50f7613135640ac04bff1d248c3ed
BradleyJrichardson <brdl.richardson@gmail.com>
2020-01-17 12:23:26 +1100

    circles

---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: #222;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.section`
+  background: #777;
+  width: 90vw;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Svg = styled.svg`
+  height: 90%;
+  width: 100%;
+`;
